1058d84076abf949d2ff400a6aa679b0662a4a98
alvinyanson <a.yanson@arcanys.com>
2024-07-24 18:06:12 +0800

    feat: WIP RabbitMQ

---
+++ ProductCatalogService/AsyncDataServices/IMessageBusClient.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductCatalogService.AsyncDataServices
+{
+    public interface IMessageBusClient
+    {
+        // when user register, maybe send an email for welcome message and special offers
+        // triggerred from ProductCatalogService
+        void UserSignUp(string email);
+    }
+}
