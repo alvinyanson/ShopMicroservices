01ebac3e854f6aa0add3ba549dfe8a66bc0462ff
alvinyanson <a.yanson@arcanys.com>
2024-07-27 12:20:50 +0800

    feat: feat initial login view and auth controller

---
+++ ShopWebApp/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShopWebApp.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction(nameof(Login));
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
