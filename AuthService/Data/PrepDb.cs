fbe91184b88060a8d917d14b2cf94711e064205a
alvinyanson <a.yanson@arcanys.com>
2024-07-29 13:15:55 +0800

    chore: Added screenshot for documentation

--- AuthService/Data/PrepDb.cs
+++ AuthService/Data/PrepDb.cs
@@ -33,23 +33,6 @@ namespace AuthService.Data
 
 
             Console.WriteLine($"isProd: {isProd}");
-
-            //if(!context.Platforms.Any())
-            //{
-            //    Console.WriteLine("Seeding data...");
-
-            //    context.Platforms.AddRange(
-            //        new Platform() { Name = "Dot Net", Publisher = "Microsoft", Cost = "Free"},
-            //        new Platform() { Name = "Sql Server Database", Publisher = "Microsoft", Cost = "Free" },
-            //        new Platform() { Name = "Kubernetes", Publisher = "Cloud Native Computing Foundation", Cost = "Free" }
-            //    );
-
-            //    context.SaveChanges();
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Data already available...");
-            //}
         }
     }
 }
